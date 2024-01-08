4d1ecbec0b9f2815d27c53d06099047a62c1ad0f
Maciej Sienkiewicz <maciej@niczyja.pl>
2024-01-08 23:15:07 +0100

    break down to separate files

---
+++ src/settings.ts
@@ -0,0 +1,54 @@
+
+import ArcSidebar from 'main';
+import { App, PluginSettingTab, Setting } from 'obsidian';
+import { TextInputModal } from 'text_input_modal';
+import { homedir } from 'os';
+
+export interface ArcSidebarSettings {
+	jsonPath: string;
+}
+
+export const DEFAULT_SETTINGS: ArcSidebarSettings = {
+	jsonPath: homedir() + '/Library/Application Support/Arc/StorableSidebar.json',
+}
+
+export class ArcSidebarSettingsTab extends PluginSettingTab {
+	plugin: ArcSidebar;
+
+	constructor(app: App, plugin: ArcSidebar) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	async display(): Promise<void> {
+		this.containerEl.empty();
+
+		const jsonExists = await this.plugin.validateJsonPath();
+
+		new Setting(this.containerEl)
+			.setName('Sidebar JSON file location')
+			.setDesc(this.plugin.settings.jsonPath)
+			.addExtraButton(cb => cb
+				.setIcon('edit')
+				.setTooltip('Change file location')
+				.onClick(async () => {
+					new TextInputModal(this.app, 'Set JSON file path', this.plugin.settings.jsonPath, async (value) => {
+						this.plugin.settings.jsonPath = value;
+						this.save();
+					}).open();
+				}))
+			.addExtraButton(cb => cb
+				.setIcon('reset')
+				.setTooltip('Reset to default')
+				.onClick(async () => {
+					this.plugin.settings.jsonPath = DEFAULT_SETTINGS.jsonPath;
+					this.save();
+				}))
+			.descEl.setAttribute('style', jsonExists ? 'color: var(--color-green);' : 'color: var(--color-red);');
+	}
+
+	async save(): Promise<void> {
+		await this.plugin.saveSettings();
+		await this.display();
+	}
+}
---
+++ src/text_input_modal.ts
@@ -0,0 +1,41 @@
+
+import { App, Modal, Setting } from 'obsidian';
+
+export class TextInputModal extends Modal {
+	name: string;
+	value: string;
+	save: (value: string) => void
+
+	constructor(app: App, name: string, value: string, save: (value: string) => void) {
+		super(app);
+		this.name = name;
+		this.value = value;
+		this.save = save;
+	}
+
+	display(): void {
+		this.titleEl.setText(this.name);
+		new Setting(this.contentEl)
+			.addText(ct => ct
+				.setValue(this.value)
+				.onChange(value =>
+					this.value = value
+				)
+				.inputEl.setAttribute('size', '60')
+			)
+			.addButton(cb => cb
+				.setButtonText('Save')
+				.onClick(async () => {
+					this.save(this.value);
+					this.close();
+				}));
+	}
+
+	onOpen() {
+		this.display();
+	}
+
+	onClose() {
+		this.contentEl.empty();
+	}
+}
