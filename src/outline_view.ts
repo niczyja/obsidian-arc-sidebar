8a50b251cf4eb61b6849c7fe8413b556e8f2964d
Maciej Sienkiewicz <maciej@niczyja.pl>
2024-01-23 00:53:52 +0100

    work out outline element structure based on file explorer view

--- src/outline_view.ts
+++ src/outline_view.ts
@@ -1,6 +1,6 @@
 
 import ArcSidebar from 'main';
-import { ItemView, WorkspaceLeaf } from 'obsidian';
+import { getIcon, ItemView, WorkspaceLeaf } from 'obsidian';
 
 export const VIEW_TYPE_OUTLINE = 'arc-sidebar-outline-view';
 
@@ -10,6 +10,7 @@ export class ArcSidebarOutlineView extends ItemView {
 	constructor(plugin: ArcSidebar, leaf: WorkspaceLeaf) {
 		super(leaf);
 		this.plugin = plugin;
+		this.navigation = false;
 	}
 
 	getViewType(): string {
@@ -25,9 +26,79 @@ export class ArcSidebarOutlineView extends ItemView {
 	}
 
 	 protected async onOpen(): Promise<void> {
-	 	const container = this.containerEl.children[1];
-	 	container.empty();
-	 	container.createEl('h4', { text: 'Arc Sidebar Outline' });
+	 	const container = this.containerEl;
+
+	 	const menuEl = container
+	 		.createDiv({ cls: 'nav-header', prepend: true })
+	 		.createDiv({ cls: 'nav-buttons-container' });
+
+	 	menuEl
+	 		.createDiv({
+	 			cls: 'clickable-icon nav-action-button',
+	 			attr: { 'aria-label': 'Show search filter' } 
+	 		}, (el) => el.onClickEvent((ev) => {
+	 			console.log(el, ev);
+	 		}))
+	 		.appendChild(<Node>getIcon('search'));
+
+	 	menuEl
+	 		.createDiv({
+	 			cls: 'clickable-icon nav-action-button',
+	 			attr: { 'aria-label': 'Change sort order' }
+	 		}, (el) => el.onClickEvent((ev) => {
+	 			console.log(el, ev);
+	 		}))
+	 		.appendChild(<Node>getIcon('arrow-up-narrow-wide'));
+
+	 	menuEl
+	 		.createDiv({
+	 			cls: 'clickable-icon nav-action-button',
+	 			attr: { 'aria-label': 'Expand all' }
+	 		}, (el) => el.onClickEvent((ev) => {
+	 			console.log(el, ev);
+	 		}))
+	 		.appendChild(<Node>getIcon('chevrons-up-down'));
+
+	 	container.removeChild(container.children[2]);
+	 	const explorerEl = container.createDiv({ cls: 'nav-files-container' });
+
+	 	// for each space
+	 	const spaceEl = explorerEl
+	 		.createDiv({ cls: 'tree-item nav-folder mod-root' });
+	 	spaceEl
+	 		.createDiv({ cls: 'tree-item-self nav-folder-title' })
+	 		.createDiv({ cls: 'tree-item-inner nav-folder-title-content', text: 'Space name' });
+	 	const rootEl = spaceEl
+	 		.createDiv({ cls: 'tree-item-children nav-folder-children' })
+
+	 	// folder example
+	 	const folderEl = rootEl
+	 		.createDiv({ cls: 'tree-item nav-folder is-collapsed' });
+
+	 	const folderTitleEl = folderEl
+	 		.createDiv({ cls: 'tree-item-self is-clickable nav-folder-title' });
+
+	 	folderTitleEl
+	 		.createDiv({ cls: 'tree-item-icon collapse-icon nav-folder-collapse-indicator is-collapsed' })
+	 		.appendChild(<Node>getIcon('right-triangle'));
+	 	folderTitleEl
+	 		.createDiv({ cls: 'tree-item-inner nav-folder-title-content', text: 'Folder name' });
+
+	 	const folderChildrenEl = folderEl
+	 		.createDiv({ cls: 'tree-item-children nav-folder-children' });
+
+	 	// folder item example
+	 	folderChildrenEl
+	 		.createDiv({ cls: 'tree-item nav-file' })
+	 		.createDiv({ cls: 'tree-item-self is-clickable nav-file-title' })
+	 		.createDiv({ cls: 'tree-item-inner nav-file-title-content', text: 'Child item name' })
+
+	 	// root item example
+	 	rootEl
+	 		.createDiv({ cls: 'tree-item nav-file' })
+	 		.createDiv({ cls: 'tree-item-self is-clickable nav-file-title' })
+	 		.createDiv({ cls: 'tree-item-inner nav-file-title-content', text: 'Root item name' })
+
 	 }
 
 	 protected async onClose(): Promise<void> {
